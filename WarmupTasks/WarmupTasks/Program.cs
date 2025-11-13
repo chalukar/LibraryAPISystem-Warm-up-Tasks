2e27bab3fb053c287740c200ce8b592dccfa93a8
Chaluka Rathnayake <chaluka.rathnayaka@gmail.com>
2025-11-14 00:49:51 +0100

    create IsPowerOfTwo, ReversBookTitle,RepeatBookTitle,ListOddNumbersId

---
+++ WarmupTasks/WarmupTasks/Program.cs
@@ -0,0 +1,29 @@
+﻿using WarmupTasks.Services;
+
+var service = new WarmUpTaskService();
+
+// 1. Power of Two
+Console.WriteLine(" 1. Check if Book ID is a power of 2:");
+Console.WriteLine($"2 - {service.IsPowerOfTwo(2)}");
+Console.WriteLine($"8 - {service.IsPowerOfTwo(8)}");
+Console.WriteLine($"10 - {service.IsPowerOfTwo(10)}");
+Console.WriteLine($"24 - {service.IsPowerOfTwo(24)}");
+Console.WriteLine();
+
+// 2. Reverse a Book Title
+// (Example: "Microservice in.NET Core" → "eroC TEN.ni ecivresorciM")
+// (Example: "Moby Dick" → "kciD yboM")
+Console.WriteLine("2. Check Reverse Book Title:");
+Console.WriteLine(service.ReversBookTitle("Microservice in.NET Core"));
+Console.WriteLine(service.ReversBookTitle("Moby Dick")); 
+Console.WriteLine();
+
+// 3. Generate Book Title Replicas
+// (Example: ("Read", 4) → "ReadReadRead")
+Console.WriteLine("3. Book Title Replicas:");
+Console.WriteLine(service.RepeatBookTitle("Read", 4));
+Console.WriteLine();
+
+// 4. List Odd-Numbered IDs (0–100)
+Console.WriteLine("4. Odd-numbered IDs:");
+service.ListOddNumbersId(0, 100);
---
+++ WarmupTasks/WarmupTasks/Services/WarmUpTaskService.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace WarmupTasks.Services
+{
+    public class WarmUpTaskService
+    {
+        // 1. Check if a Book ID is a power of 2
+        public bool IsPowerOfTwo(int id)
+        {
+            
+            if (id <= 0) return false;
+            return (id & (id - 1)) == 0;
+        }
+
+        // 2. Reverse a Book Title
+        // (Example: "Moby Dick" → "kciD yboM")
+        public string ReversBookTitle(string bookTitle) { 
+            if(string.IsNullOrEmpty(bookTitle))
+            {
+                return bookTitle;
+            }
+            else
+            {
+                char[] bktl = bookTitle.ToCharArray();
+                Array.Reverse(bktl);
+                return new string(bktl);
+            }
+        }
+
+        //3. Generate Book Title Replicas
+        // (Example: ("Read", 4) → "ReadReadReadRead")
+        public string RepeatBookTitle(string bookTitle,int count)
+        {
+            if (count <= 0)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return string.Concat(Enumerable.Repeat(bookTitle, count));
+            }
+        }
+
+        // 4. List Odd-Numbered Book IDs between 0 and 100
+        public void ListOddNumbersId(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
